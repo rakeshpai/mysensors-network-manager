5d8198f2479c7f0e2057df41a984af6d73ccce92
Rakesh Pai <rakeshpai@gmail.com>
2017-07-26 19:03:07 +0530

    Upgraded deps, made modifications according to changelog at https://github.com/facebookincubator/create-react-app/blob/master/CHANGELOG.md

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+
+import './index.css';
+
 import App from './components/App';
 
 import { Provider } from 'react-redux';
